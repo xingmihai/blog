b630131a634f9d172b252fbdf3d09aa346ae8743
xingmihai <1498934815@gmail.com>
2026-07-22 14:51:57 +0800

    update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -959,7 +959,7 @@ function initWaline(path) {
   window.WalineInit({
     el: '#waline',
     serverURL: CONFIG.walineServer,
-    path: `#/post/${path}`,
+    path: `/post/${path}`,
     dark: 'html.mdui-theme-dark',
     lang: 'zh-CN',
   });
